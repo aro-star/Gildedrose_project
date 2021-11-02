b55307a20c37345d125baab6e1da73e425635418
aro-star <85771488+aro-star@users.noreply.github.com>
2021-11-02 14:14:15 -0700

    Add files via upload

--- texttest_fixture.py
+++ texttest_fixture.py
@@ -8,7 +8,12 @@ if __name__ == "__main__":
     items = [
              Item(name="+5 Dexterity Vest", sell_in=10, quality=20),
              Item(name="Aged Brie", sell_in=2, quality=0),
-             Item(name="Elixir of the Mongoose", sell_in=5, quality=7),
+             Item(name="Aged Brie", sell_in=0, quality=0),
+             Item(name="Aged Brie", sell_in=-10, quality=0),
+             Item(name="Aged Brie", sell_in=2, quality=50),
+             Item(name="Aged Brie", sell_in=2, quality=52),
+             Item(name="Aged Brie", sell_in=2, quality=-5),
+             Item(name="Elixir of the Mongoose", sell_in=1, quality=1),
              Item(name="Sulfuras, Hand of Ragnaros", sell_in=0, quality=80),
              Item(name="Sulfuras, Hand of Ragnaros", sell_in=-1, quality=80),
              Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=15, quality=20),
