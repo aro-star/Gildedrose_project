fca70cc10488a55cb17bc584907107184099a13b
aro-star <85771488+aro-star@users.noreply.github.com>
2021-11-02 21:08:34 -0700

    Add files via upload
    refactored the code of gilded_rose.py

--- gilded_rose.py
+++ gilded_rose.py
@@ -1,4 +1,8 @@
-# -*- coding: utf-8 -*-
+MIN_QUALITY = 0
+MAX_QUALITY = 50
+LEGENDARY_MAX_QUALITY = 80
+MIN_SELL_IN_VALUE = 0
+
 
 class GildedRose(object):
 
@@ -7,33 +11,7 @@ class GildedRose(object):
 
     def update_quality(self):
         for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
-            else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
+            item.update_item()
 
 
 class Item:
@@ -44,3 +22,77 @@ class Item:
 
     def __repr__(self):
         return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
+
+
+class NormalItem(Item):
+    def __init__(self, name, sell_in, quality):
+        Item.__init__(self, name, sell_in, quality)
+
+    def update_item(self):
+        self._update_sell_in()
+        self._update_quality()
+
+    def _update_quality(self):
+        if self.sell_in >= 0:
+            return max(self.quality - 1, MIN_QUALITY)
+        else:
+            return max(self.quality - 2, MIN_QUALITY)
+
+    def _update_sell_in(self) -> None:
+        self.sell_in -= 1
+
+
+class AgedItem(NormalItem):
+    def __init__(self, name, sell_in, quality):
+        NormalItem.__init__(self, name, sell_in, quality)
+
+    def _update_quality(self):
+        if self.quality < 0:
+            return MIN_QUALITY
+
+        if sell_in >= 0 and self.quality > 0:
+            return min(self.quality + 1, MAX_QUALITY)
+        else:
+            return min(self.quality + 2, MAX_QUALITY)
+
+
+class LegendaryItem(NormalItem):
+    def __init__(self, name, sell_in, quality):
+        NormalItem.__init__(self, name, sell_in, quality)
+
+    def _update_quality(self) -> int:
+        if self.quality > LEGENDARY_MAX_QUALITY:
+            return LEGENDARY_MAX_QUALITY
+        else:
+            return LEGENDARY_MAX_QUALITY
+
+    def _update_sell_in(self) -> None:
+        pass
+
+
+class TicketItem(NormalItem):
+    def __init__(self, name, sell_in, quality):
+        NormalItem.__init__(self, name, sell_in, quality)
+
+    def _update_quality(self):
+        if sell_in > 10:
+            return min(self.quality + 1, MAX_QUALITY)
+        elif sell_in > 5:
+            return min(self.quality + 2, MAX_QUALITY)
+        elif sell_in >= 0:
+            return min(self.quality + 3, MAX_QUALITY)
+        else:
+            return MIN_QUALITY
+
+
+class ConjuredItem(NormalItem):
+    def __init(self, name, sell_in, quality):
+        NormalItem.__init__(self, name, sell_in, quality)
+
+    def _update_quality(self):
+        if self.quality < 0:
+            return MIN_QUALITY
+        if self.sell_in >= 0:
+            return max(self.quality - 2, MIN_QUALITY)
+        else:
+            return max(self.quality - 4, MIN_QUALITY)
