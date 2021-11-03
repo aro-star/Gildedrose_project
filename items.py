547455ffe0dad1d2d24f96b23a1dc14a525fb373
aro-star <85771488+aro-star@users.noreply.github.com>
2021-11-03 15:13:56 -0700

    Add files via upload

---
+++ items.py
@@ -0,0 +1,7 @@
+import enum
+
+class Products(enum.Enum):
+    AGED_BRIE = "Aged Brie"
+    BACKSTAGE_PASSES = "BackStage Passes to a TAFKAL80ETC concert"
+    SULFURAS = "Sulfuras, Hand of Ragnaros"
+    CONJURED = "Conjured Mana Cake"
